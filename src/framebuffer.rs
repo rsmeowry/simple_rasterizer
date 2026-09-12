186f083753156f90bbf66ae27645448bee15dcbd
rosemeowry <rosemary.dev@proton.me>
2026-09-12 19:13:25 +0300

    basic framebuffer ig

---
+++ src/framebuffer.rs
@@ -0,0 +1,25 @@
+﻿pub struct Framebuffer {
+    width: usize,
+    height: usize,
+    color: Vec<u32>,
+    depth: Vec<f32>
+}
+
+impl Framebuffer {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            height,
+            color: vec![0xF2_00_00; width * height],
+            depth: vec![f32::INFINITY; width * height],
+        }
+    }
+
+    pub fn color(&self) -> &[u32] {
+        &self.color
+    }
+
+    pub fn clear_depth(&mut self) {
+        self.depth.fill(f32::INFINITY);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 pub mod tri;
 mod util;
+mod framebuffer;
 
 use glam::{Vec2, Vec3};
+use minifb::{Key, Scale, ScaleMode, WindowOptions};
+use crate::framebuffer::Framebuffer;
 
 fn main() {
+    let mut win = minifb::Window::new("my rasterizer! :D", 800, 640, WindowOptions {
+        borderless: false,
+        title: true,
+        resize: false,
+        scale: Scale::FitScreen,
+        scale_mode: ScaleMode::Center,
+        topmost: false,
+        transparency: false,
+        none: false,
+    }).unwrap();
+
+    let mut fb = Framebuffer::new(800, 640);
+
+    while win.is_open() && !win.is_key_down(Key::Escape) {
+        win.update_with_buffer(fb.color(), 800, 640).unwrap();
+    }
 }
